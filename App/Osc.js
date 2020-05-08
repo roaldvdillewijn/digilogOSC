e75a5c2022b70e42f23052bba344680d3c4e488f
Roald van Dillewijn <info@roaldvandillewijn.nl>
2020-05-08 22:23:00 +0200

    added upgrades for fade&oscillate

--- App/Osc.js
+++ App/Osc.js
@@ -19,10 +19,10 @@ class Osc {
         
         //get the pedal+param-address of the message
         let addr = msg[0];
-        let value = (val.length==1)?val[0]:val;
+        let value = (val.length == 1) ? val[0] : val;
         let func = msg[0].split("/")[2];
         if (this.funcs.includes(func)) {
-          addr = "/"+msg[0].split("/")[1]+"/"+val[0];
+          addr = "/" + msg[0].split("/")[1] + "/" + val[0];
         }
         
         if (func == "fade" || func == "oscillate") {
@@ -30,35 +30,44 @@ class Osc {
         }
 
         //check if any hold-function (once || change) should be switched off
-        if (val[val.length -1] != "once" && val[val.length -1] != "change") {
+        if (val[val.length - 1] != "once" && val[val.length - 1] != "change") {
           this.state[addr] = 0;
         }
-        if (val[val.length-1] == "change" && this.value[addr] != value[0]) {
+        if (val[val.length - 1] == "change" && this.value[addr] != value[0]) {
+          this.state[addr] = 0;
+        }
+        if (val[val.length - 1] == "cont" && func == "fade") {
           this.state[addr] = 0;
         }
 
         //if no hold function, return the incoming OSC-data. 
         if (!this.state[addr] && func != "stop") {
-          (val.length == 4 && msg[0].split("/")[2] == "ramp")?val.push(50):null;
-          let returndata = {"pedal":0,"param":0,"value":value};
+          (val.length == 4 && msg[0].split("/")[2] == "ramp") ? val.push(50) : null;
+          let returndata = {"pedal":0, "param":0, "value":value};
           returndata.pedal = msg[0].split("/")[1];
           returndata.param = msg[0].split("/")[2];
           callback(returndata,msg);
         }
         else if (!this.stop[addr] && func == "stop") {
-          let returndata = {"pedal":0,"param":0,"value":value};
+          let returndata = {"pedal":0, "param":0, "value":value};
           returndata.pedal = msg[0].split("/")[1];
           returndata.param = msg[0].split("/")[2];
           callback(returndata,msg);
         }
 
         //if the osc message asks for new or updated hold function, do so.
-        if (val[val.length - 1] == "once") {
+        if (val[val.length - 1] == "once" && func == "fade") {
           this.state[addr] = 1;
         }
-        if (val[val.length -1] == "change") {
+        if (val[val.length - 1] == "once" && func == "oscillate") {
+          this.state[addr] = 3;
+        }
+        if (val[val.length - 1] == "change") {
           this.state[addr] = 2;
           this.value[addr] = value[0];
+        }
+        if (val[val.length - 1] == "loop") {
+          this.state[addr] = 4;
         }
         if (func == "stop") {
           this.stop[addr] = 1;
@@ -67,7 +76,9 @@ class Osc {
     }
   }
   setState(id) {
-    this.state[id] = 0;
+    if (this.state[id] == 3 || this.state[id] == 4) {
+      this.state[id] = 0;  
+    }
   }
 
   createClient() {
